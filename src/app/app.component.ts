6ff815f8a80fe4fa6ef98e870a19e033f21d161b
Jose Gutierrez <josepmind@gmail.com>
2021-07-01 21:22:54 -0400

    working on angular form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
-
+import { FormBuilder, FormGroup, FormControl, Validators} from '@angular/forms';
+  
+import { ConfirmedValidator } from './confirmed.validator';
+    
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'testform2';
+  form: FormGroup = new FormGroup({});
+  
+  constructor(private fb: FormBuilder) {
+  
+    this.form = fb.group({
+      password: ['', [Validators.required]],
+      confirm_password: ['', [Validators.required]]
+    }, { 
+      validator: ConfirmedValidator('password', 'confirm_password')
+    })
+  }
+    
+  get f(){
+    return this.form.controls;
+  }
+   
+  submit(){
+    console.log(this.form.value);
+  }
+   
 }
