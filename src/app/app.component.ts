e66adb7ad0cbf508cb4566b876b435edb099656f
Jose Gutierrez <josepmind@gmail.com>
2021-07-05 21:08:47 -0400

    finish steps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit, ViewChild, ElementRef, HostListener } from '@angular/core';
 import { FormBuilder, FormGroup, FormControl, Validators} from '@angular/forms';
 import { ApiService } from './shared/api.service';
 import { ConfirmedValidator } from './confirmed.validator';
@@ -10,19 +10,24 @@ import { FormModel } from './formdata.model';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  form: FormGroup = new FormGroup({});
+
+  armediaform: FormGroup;
   
   formModelObj : FormModel = new FormModel();
   
+  listData: any;
 
+ 
 
   constructor(private fb: FormBuilder,
     private api : ApiService) {
   
-    this.form = fb.group({
-      email: new FormControl(''),
-      subscription: new FormControl(''),
-      date: new FormControl(''),
+      this.listData = [];
+
+    this.armediaform = this.fb.group({
+      email: ['', Validators.required],
+      subscription: ['Advanced', Validators.required],
+      date: [(new Date()).toISOString().substring(0,10)],
       password: ['', [Validators.required]],
       confirm_password: ['', [Validators.required]]
     },  { 
@@ -31,22 +36,39 @@ export class AppComponent {
   }
     
   get f(){
-    return this.form.controls;
+    return this.armediaform.controls;
   }
-   
-  onSubmit(){
-    this.formModelObj.email = this.form.value.email;
-    this.formModelObj.subscription = this.form.value.subscription;
-    this.formModelObj.password = this.form.value.password;
-    this.formModelObj.date = this.form.value.date;
+
+
+  addForm(){
+
+    this.formModelObj.email = this.armediaform.value.email;
+    this.formModelObj.subscription = this.armediaform.value.subscription;
+    this.formModelObj.password = this.armediaform.value.password;
+    this.formModelObj.date = this.armediaform.value.date;
 
     this.api.postData(this.formModelObj)
     .subscribe((res: any)=>{
       console.log(res);
       alert('data added successfully')
-      this.form.reset();
+      this.armediaform.reset();
     })
-    console.log(this.form.value);
+    console.log(this.armediaform.value);
+
+    this.listData.push(this.armediaform.value);
+   
   }
+
+  reset(){
+    if (confirm('Are you sure you want to discard the changes?')) {
+      this.armediaform.reset();
+      console.log('form was reset')
+    } else {
+      console.log('form was not reset')
+    }
+    
+  }
+  
+
   
 }
